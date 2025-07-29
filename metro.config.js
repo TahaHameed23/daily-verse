2538a345bd3653876003c2d9bf83477498057f5f
TahaHameed23 <89855847+TahaHameed23@users.noreply.github.com>
2025-07-29 17:04:07 +0530

    Fix metro config for proper bundling

--- metro.config.js
+++ metro.config.js
@@ -4,7 +4,6 @@ const config = getDefaultConfig(__dirname);
 
 // Enable Metro bundler optimizations
 config.transformer.minifierConfig = {
-  keep_quoted_props: true,
   mangle: {
     keep_fnames: true,
   },
@@ -12,8 +11,5 @@ config.transformer.minifierConfig = {
 
 // Enable tree shaking
 config.resolver.platforms = ['native', 'android', 'ios', 'web'];
-
-// Optimize asset resolution
-config.transformer.assetPlugins = ['expo-asset/tools/hashAssetFiles'];
 
 module.exports = config;
